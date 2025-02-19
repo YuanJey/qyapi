fce97946128d5c3e4967dacca10257fb1002ed43
yuanjey <2218773049@qq.com>
2025-02-19 16:36:27 +0800

    4

--- api/api.go
+++ api/api.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/YuanJey/qyapi/http_client"
 	"github.com/YuanJey/qyapi/resp"
+	"net/url"
 )
 
 const (
 	getAccessToken    = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s"
-	getDepartmentList = "https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=%s"
-	getUserList       = "https://qyapi.weixin.qq.com/cgi-bin/user/simplelist?access_token=%s&department_id=%s"
+	getDepartmentList = "https://qyapi.weixin.qq.com/cgi-bin/department/list?%s"
+	getUserList       = "https://qyapi.weixin.qq.com/cgi-bin/user/simplelist?%s"
 )
 
 type QYApi struct {
@@ -38,17 +39,21 @@ func (q *QYApi) ReSetAccessToken() error {
 // GetDepartmentList 获取部门列表
 func (q *QYApi) GetDepartmentList(did string) (*resp.GetDepartmentListResp, error) {
 	getDepartmentListResp := resp.GetDepartmentListResp{}
-	url := fmt.Sprintf(getDepartmentList, q.AccessToken)
+	values := url.Values{}
 	if did != "" {
-		url += fmt.Sprintf("&id=%s", did)
+		values.Set("id", did)
 	}
-	err := http_client.Get(url, nil, &getDepartmentListResp)
+	values.Set("access_token", q.AccessToken)
+	err := http_client.Get(fmt.Sprintf(getDepartmentList, values.Encode()), nil, &getDepartmentListResp)
 	return &getDepartmentListResp, err
 }
 
 // GetUserList 获取部门成员
 func (q *QYApi) GetUserList(did string) (*resp.GetUserListResp, error) {
 	getUserListResp := resp.GetUserListResp{}
-	err := http_client.Get(fmt.Sprintf(getUserList, q.AccessToken, did), nil, &getUserListResp)
+	values := url.Values{}
+	values.Set("department_id", did)
+	values.Set("access_token", q.AccessToken)
+	err := http_client.Get(fmt.Sprintf(getUserList, values.Encode()), nil, &getUserListResp)
 	return &getUserListResp, err
 }
